97243062b70ddceaf2d1fec5e9ebb55e834ecdd8
AlexEgorushkin <shura.reno.97@bk.ru>
2025-01-10 13:11:17 +0300

    change remove func & add alt to image

--- scripts/index.js
+++ scripts/index.js
@@ -8,15 +8,16 @@ const placesContainer = document.querySelector('.places__list');
 const createNewCard = (data, removeCardFunction) => {
   const newCard = cardTemplate.querySelector('.card').cloneNode(true);
   newCard.querySelector('.card__image').src = data.link;
+  newCard.querySelector('.card__image').alt = data.name;
   newCard.querySelector('.card__title').textContent = data.name;
 
-  newCard.querySelector('.card__delete-button').addEventListener('click', removeCardFunction)
+  newCard.querySelector('.card__delete-button').addEventListener('click', () => removeCardFunction(newCard))
 
   return newCard;
 }
 
 // @todo: Функция удаления карточки
-const removeCard = (event) => event.target.closest('.card').remove()
+const removeCard = (card) => card.remove();
 
 // @todo: Вывести карточки на страницу
 initialCards.forEach(cardData => placesContainer.append(createNewCard(cardData, removeCard)));
