6dbe6f3ae2e950afa23d8c8093d7d4d96dd44c3f
AlexEgorushkin <shura.reno.97@bk.ru>
2025-01-13 09:44:19 +0300

    Вынес ранее реализованные фукнции для карточки в card, подключение оставил в index.js, добавил файл modal.js для фукнций модального окна

---
+++ src/components/card.js
@@ -0,0 +1,27 @@
+// Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
+
+// DOM узлы
+const placesContainer = document.querySelector('.places__list');
+
+// Функция создания карточки
+const createNewCard = (data, removeCardFunction) => {
+  const newCard = cardTemplate.querySelector('.card').cloneNode(true);
+  newCard.querySelector('.card__image').src = data.link;
+  newCard.querySelector('.card__image').alt = data.name;
+  newCard.querySelector('.card__title').textContent = data.name;
+
+  newCard.querySelector('.card__delete-button').addEventListener('click', () => removeCardFunction(newCard))
+
+  return newCard;
+}
+
+// Функция удаления карточки
+const removeCard = (card) => card.remove();
+
+export {
+    cardTemplate,
+    placesContainer,
+    createNewCard,
+    removeCard
+}
