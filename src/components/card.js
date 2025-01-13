664ea97ec463180c973d384ce8c61b533d6f8364
AlexEgorushkin <shura.reno.97@bk.ru>
2025-01-13 11:04:59 +0300

    Добавил функцию добавления лайка

--- src/components/card.js
+++ src/components/card.js
@@ -2,21 +2,30 @@
 const cardTemplate = document.querySelector('#card-template').content;
 
 // Функция создания карточки
-const createNewCard = (data, removeCardFunction) => {
-  const newCard = cardTemplate.querySelector('.card').cloneNode(true);
-  newCard.querySelector('.card__image').src = data.link;
-  newCard.querySelector('.card__image').alt = data.name;
-  newCard.querySelector('.card__title').textContent = data.name;
+const createNewCard = (data, removeCardFunction, toggleLikeCardFunction) => {
+    const newCard = cardTemplate.querySelector('.card').cloneNode(true)
+    const deleteButton = newCard.querySelector('.card__delete-button')
+    const likeButton = newCard.querySelector('.card__like-button')
 
-  newCard.querySelector('.card__delete-button').addEventListener('click', () => removeCardFunction(newCard))
+    newCard.querySelector('.card__image').src = data.link;
+    newCard.querySelector('.card__image').alt = data.name;
+    newCard.querySelector('.card__title').textContent = data.name;
 
-  return newCard;
+    deleteButton.addEventListener('click', () => removeCardFunction(newCard))
+    likeButton.addEventListener('click', () => toggleLikeCardFunction(likeButton))
+
+    return newCard;
 }
 
 // Функция удаления карточки
 const removeCard = (card) => card.remove();
 
+// Функция добавления лайка карточке
+const toggleLikeCard = (cardLikeButton) => cardLikeButton.classList.toggle('card__like-button_is-active');
+
+
 export {
     createNewCard,
-    removeCard
+    removeCard,
+    toggleLikeCard
 }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import "./pages/index.css";
 import initialCards from "./scripts/cards";
 import {
     createNewCard,
-    removeCard
+    removeCard,
+    toggleLikeCard
 } from './components/card'
 
 // Контейнер для карточек
 const placesContainer = document.querySelector('.places__list');
 
-initialCards.forEach(cardData => placesContainer.append(createNewCard(cardData, removeCard)));
+initialCards.forEach(cardData => placesContainer.append(createNewCard(cardData, removeCard, toggleLikeCard)));
